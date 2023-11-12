4ec46b08dd4f574a709c0d874be31c8892cd6b5f
Mateus Pina <mateusdepinanascimento@usp.br>
2023-11-12 17:24:37 -0300

    Commit 3

--- noticias/urls.py
+++ noticias/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 from .views import home, post_list, post_detail, post_create, post_edit, post_delete, post_new
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('posts/', PostListView.as_view(), name='post_list'),
+    path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('new/', PostCreateView.as_view(), name='post_create'),
+    path('<int:pk>/edit/', PostUpdateView.as_view(), name='post_update'),
+    path('<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
     path('', home, name='home'),
-    path('posts/', post_list, name='post_list'),
-    path('<int:pk>/', post_detail, name='post_detail'),
-    path('new/', post_create, name='post_create'),
-    path('<int:pk>/edit/', post_edit, name='post_edit'),
-    path('post/new/', post_new, name='post_create'),
-    path('<int:pk>/delete/', post_delete, name='post_delete'),
 ]
 
 if settings.DEBUG:
--- noticias/views.py
+++ noticias/views.py
@@ -2,7 +2,35 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'noticias/post_list.html'
+    context_object_name = 'posts'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'noticias/post_detail.html'
+    context_object_name = 'post'
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'noticias/post_form.html'
+    success_url = reverse_lazy('post_list')
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'noticias/post_form.html'
+    success_url = reverse_lazy('post_list')
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'noticias/post_confirm_delete.html'
+    success_url = reverse_lazy('post_list')
 
 def home(request):
     return render(request, 'noticias/home.html')
