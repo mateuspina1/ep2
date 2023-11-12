59021f149a8df9388e25155a7a58ed2ea20551f3
Mateus Pina <mateusdepinanascimento@usp.br>
2023-11-12 12:58:19 -0300

    Adicionando suporte a imagem

--- noticias/models.py
+++ noticias/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    image = models.ImageField(upload_to='post_images/', null=True, blank=True)
     published_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
